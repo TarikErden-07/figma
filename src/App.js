938566387c37e41af1910cd5ff66ed66645ed7d4
TarıkErden-07 <txrxkxrdxn@gmail.com>
2023-07-28 10:56:32 +0300

    Figma Css Ornek

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    salary: ''
+  });
+
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = () => {
+    console.log('Form Data:', formData);
+    setSubmittedData(formData);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="containers">
+          
+          <div className="container">
+            <a className='link' href='link'>Hakkımızda</a>
+          </div>
+
+          
+          <div className='container2'>
+            
+            {submittedData ? (
+              <div>
+                <p>Adınız: {submittedData.name}</p>
+                <p>E-posta Adresiniz: {submittedData.email}</p>
+                <p>Telefon Numaranız: {submittedData.phone}</p>
+                <p>Maaşınız: {submittedData.salary}</p>
+              </div>
+            ) : (
+              <p>Bilgileri doldurun ve "Gir" butonuna tıklayın.</p>
+            )}
+          </div>
+
+          
+          <div className='container3'>
+            <div className='input'>
+              <p>Adınızı giriniz</p><input type="text" name="name" onChange={handleChange} />
+              <br />
+              <p>E-posta adresiniz</p><input type="email" name="email" onChange={handleChange} />
+              <br />
+              <p>Telefon numaranız</p><input type="tel" name="phone" onChange={handleChange} />
+              <br />
+              <p>Maaşınız</p><input type="text" name="salary" onChange={handleChange} />
+            </div>
+            <button className='btn' onClick={handleSubmit}>Gir</button>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
